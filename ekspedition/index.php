e28a42cdf8739f6f742f4bbf2580c7f68fdd7908
Troels <troels@plougmann-olsen.dk>
2016-08-03 20:59:36 +0200

    More beautiful code

--- ekspedition/index.php
+++ ekspedition/index.php
@@ -1,89 +1,94 @@
 <head>
-<meta http-equiv="refresh" content="60">
-<title>Arbejdsstyring</title>
-<link href="http://status.ophug1.dk/style.css" rel="stylesheet" type="text/css"></head>
+    <meta http-equiv="refresh" content="60">
+    <title>Arbejdsstyring</title>
+    <link href="http://status.ophug1.dk/style.css" rel="stylesheet" type="text/css">
+</head>
 <style>
 
-@import url(https://fonts.googleapis.com/css?family=Roboto:100,400);
+    @import url(https://fonts.googleapis.com/css?family=Roboto:100,400);
 
-html {
-  font-size : 62.5%;
-  height    : 100%;
-}
+    html {
+        font-size: 62.5%;
+        height: 100%;
+    }
 
-body {
-    min-height          : 100%;
-    min-width           : 100%;
-    background-color    : #080808;
-    background-image    : url('/images/bg.jpg');
-    background-repeat   : no-repeat;
-    background-size     : cover;
-    background-position : center bottom;
-    overflow            : hidden;
-    display             : flex;
-    justify-content     : center;
-    align-items         : center;
-}
+    body {
+        min-height: 100%;
+        min-width: 100%;
+        background-color: #080808;
+        background-image: url('/images/bg.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center bottom;
+        overflow: hidden;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 
-main {
-    width: 100%;
-    /*max-width: 35rem;*/
-    padding: 2.5rem;
-    margin-left: auto;
-    margin-right: auto;
-    font-family: 'Roboto', sans-serif;
-    font-size: 1.4rem;
-    line-height: 3rem;
-}
+    main {
+        width: 100%;
+        /*max-width: 35rem;*/
+        padding: 2.5rem;
+        margin-left: auto;
+        margin-right: auto;
+        font-family: 'Roboto', sans-serif;
+        font-size: 1.4rem;
+        line-height: 3rem;
+    }
 
-h1, h2, h3 {
-    text-align: center;
-    text-rendering: geometricPrecision;
-}
+    h1, h2, h3 {
+        text-align: center;
+        text-rendering: geometricPrecision;
+    }
 
-h1 {
-    font-size: 3.2rem;
-    line-height: 2.4em;
-    letter-spacing: 0.15em;
-    color: #0a0a0a;
-    margin-bottom: 1rem;
-    text-transform: uppercase;
-    font-family:
-}
+    h1 {
+        font-size: 3.2rem;
+        line-height: 2.4em;
+        letter-spacing: 0.15em;
+        color: #0a0a0a;
+        margin-bottom: 1rem;
+        text-transform: uppercase;
+        font-family:
+    }
 
-h2 {
-    font-size: 1.6rem;
-    line-height: 1.2em;
-    letter-spacing: 0.15em;
-    color: #0a0a0a;
-    margin-bottom: 1rem;
-    text-transform: uppercase;
-    font-family: 'Roboto', sans-serif;
-    font-weight: 100;
-}
+    h2 {
+        font-size: 1.6rem;
+        line-height: 1.2em;
+        letter-spacing: 0.15em;
+        color: #0a0a0a;
+        margin-bottom: 1rem;
+        text-transform: uppercase;
+        font-family: 'Roboto', sans-serif;
+        font-weight: 100;
+    }
 
 </style>
 </head>
 <body>
 <main>
 
-<?php
+    <?php
 
-header('Content-Type: text/html; charset=utf-8');
-?>
+    header('Content-Type: text/html; charset=utf-8');
+    ?>
 
-<h1>Ekspedition</h1><h2><FORM METHOD="LINK" ACTION="./liste/index.php">
-<button TYPE="submit" class="knap"><span><img src="/images/note32.png" height="15px" /> Arbejdsliste</span></button>
-</FORM>
-<FORM METHOD="LINK" ACTION="../">
-<button TYPE="submit" class="knap"><span><img src="/images/unlock32.png" height="15px" /> Log ud</span></button>
-</FORM>
+    <h1>Ekspedition</h1>
+    <h2>
+        <FORM METHOD="LINK" ACTION="./liste/index.php">
+            <button TYPE="submit" class="knap"><span><img src="/images/note32.png" height="15px"/> Arbejdsliste</span>
+            </button>
+        </FORM>
+        <FORM METHOD="LINK" ACTION="../">
+            <button TYPE="submit" class="knap"><span><img src="/images/unlock32.png" height="15px"/> Log ud</span>
+            </button>
+        </FORM>
 
-<div class="infobox">
-<?php include('../licens.php'); ?>
-</div>
+        <div class="infobox">
+            <?php include('../licens.php'); ?>
+        </div>
 
-</h2>
+    </h2>
 
 
 </main>
--- vaerksted/liste/index.php
+++ vaerksted/liste/index.php
@@ -1,28 +1,34 @@
 ﻿<?php require_once('../../config.php'); ?>
 <title>Arbejdsliste</title>
 <head>
-<link href="http://fonts.googleapis.com/css?family=Droid+Serif" rel="stylesheet" type="text/css">
-<link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700' rel='stylesheet' type='text/css'>
-<link href="http://status.ophug1.dk/style.css" rel="stylesheet" type="text/css">
-<meta http-equiv="refresh" content="60;url=./">
+    <link href="http://fonts.googleapis.com/css?family=Droid+Serif" rel="stylesheet" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700' rel='stylesheet' type='text/css'>
+    <link href="http://status.ophug1.dk/style.css" rel="stylesheet" type="text/css">
+    <meta http-equiv="refresh" content="60;url=./">
 </head>
 <p>Arbejdsliste</p>
 <p>
-<table><tr><td><FORM METHOD="LINK" ACTION="../index.php">
-                <button TYPE="submit" class="knap"><span><img src="/images/leftturnarrow32.png" height="15px"/> Forside</span></button>
-            </FORM></td></tr>
+<table>
+    <tr>
+        <td>
+            <FORM METHOD="LINK" ACTION="../index.php">
+                <button TYPE="submit" class="knap"><span><img src="/images/leftturnarrow32.png" height="15px"/> Forside</span>
+                </button>
+            </FORM>
+        </td>
+    </tr>
 </table></p>
 <?php
 /* connect to the db */
-$connection = mysqli_connect($dbhost,$dbuser,$dbpass,$db);
+$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
 mysqli_query($connection, "SET NAMES utf8");
 mysqli_query($connection, "SET character_set_results=’utf8′");
 
 // For the purpose of saving space on my post I'm going to limit the amount of results to 4, see "LIMIT 4" below.
-  $sql = "SELECT * FROM afmont ORDER BY id DESC";
+$sql = "SELECT * FROM afmont ORDER BY id DESC";
 
-      // lister resultater
-  include '../func_list.php';
+// lister resultater
+include '../func_list.php';
 
 $connection->close();
 ?>
