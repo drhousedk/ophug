bde01bef4fdc1bd42cdaa18ce57e87db1e84dd36
Troels <troels@plougmann-olsen.dk>
2016-08-23 09:50:29 +0200

    image support beta

--- vaerksted/func_list.php
+++ vaerksted/func_list.php
@@ -2,7 +2,7 @@
 
 
 echo "<table>";
-echo "<tr bgcolor='#333333'><td><b>Stamnr</b></td><td><b>Bil</b></td><td><b>Reservedel</b></td><td><b>Besked</b></td><td><b>Varetur</b></td><td><b>Status</b></td><td><b>Ekspedient</b></td><td><b>Mekaniker</b></td><td><b>Bestilt</b></td><td><b>Tag opgave</b></td><td><b>Meld klar</b></td><td><b>Note</b></td><td><b>Print</b></td></tr>";
+echo "<tr bgcolor='#333333'><td><b>Stamnr</b></td><td><b>Bil</b></td><td><b>Reservedel</b></td><td><b>Besked</b></td><td><b>Varetur</b></td><td><b>Status</b></td><td><b>Ekspedient</b></td><td><b>Mekaniker</b></td><td><b>Bestilt</b></td><td><b>Tag opgave</b></td><td><b>Meld klar</b></td><td><b>Note</b></td><td><b>Print</b></td><td><b>Billede</b></td></tr>";
 
 while ($row = mysqli_fetch_array($result)) {
 
@@ -18,7 +18,11 @@ while ($row = mysqli_fetch_array($result)) {
     $id = $row['id'];
     $besked = $row['besked'];
     $note = $row['note'];
+    $imgsize = $row['imgsize'];
+    $imgname = $row['imgname'];
+    $imgtype = $row['imgtype'];
 
+    if ( $imgsize > 0 ) { $imglink = "<center><a href='/getimage.php?id=$id' target='_blank'><img src='/images/camera32.png' height='20px' /></a></center>"; } else { $imglink = ""; }
 
     if ($status == "Bestilt") {
         echo "<tr bgcolor='#AB0000'><td>" . $stamnr . "</td><td>" . $bil . "</td><td>" . $reservedel . "</td><td>" . $besked . "</td><td>" . $varetur . "</td><td>" . $status . "</td><td>" . $ekspedient . "</td><td>" . $mekaniker . "</td><td>" . $tid . "</td>
@@ -37,17 +41,18 @@ while ($row = mysqli_fetch_array($result)) {
 
         $connection2->close();
 
-        echo "</td><td></td><td>" . $note . "</td><td><center><a href='/print.php?id=" . $id . "' target='_blank'><img src='/images/printer32.png' height='20px' /></a></center></td></tr>";
+        echo "</td><td></td><td>" . $note . "</td><td><center><a href='/print.php?id=" . $id . "' target='_blank'><img src='/images/printer32.png' height='20px' /></a></center></td><td>$imglink</td></tr>";
     } elseif ($status == "På vej") {
 
         echo "<tr bgcolor='#ABAB00'><td>" . $stamnr . "</td><td>" . $bil . "</td><td>" . $reservedel . "</td><td>" . $besked . "</td><td>" . $varetur . "</td><td>" . $status . "</td><td>" . $ekspedient . "</td><td>" . $mekaniker . "</td><td>" . $tid . "</td><td>Er taget</td></td><td>
-<form name='klar' method='post' action='klar_do.php?id=" . $id . "'><input type='submit' value='Meld klar!'><!--<a href=klar_do.php?id=" . $id . ">Meld klar</a>--></td><td><input type='text' name='note'></form></td><td><center><a href='/print.php?id=" . $id . "' target='_blank'><img src='/images/printer32.png' height='20px' /></a></center></td></tr>";
+<form name='klar' method='post' action='klar_do.php?id=" . $id . "'><input type='submit' value='Meld klar!'><!--<a href=klar_do.php?id=" . $id . ">Meld klar</a>--></td><td><input type='text' name='note'></form></td><td><center><a href='/print.php?id=" . $id . "' target='_blank'><img src='/images/printer32.png' height='20px' /></a></center></td><td>$imglink</td></tr>";
     } else {
 
         echo "<tr bgcolor='#2BAB00'><td>" . $stamnr . "</td><td>" . $bil . "</td><td>" . $reservedel . "</td><td>" . $besked . "</td><td>" . $varetur . "</td><td><b>" . $status . "</b></td><td>" . $ekspedient . "</td><td>" . $mekaniker . "</td><td>" . $tid . "</td><td></td><td></td><td>
-" . $note . "</td><td><center><a href='/print.php?id=" . $id . "' target='_blank'><img src='/images/printer32.png' height='20px' /></a></center></td></tr>";
+" . $note . "</td><td><center><a href='/print.php?id=" . $id . "' target='_blank'><img src='/images/printer32.png' height='20px' /></a></center></td><td>$imglink</td></tr>";
 
     }
+
 }
 
 echo "</table>";
